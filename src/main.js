42eb625d5d5c187b9dfbcdd66146606b862d1b45
Eden <gresseden@gmail.com>
2022-12-08 16:35:34 -0500

    Testing on productin

--- src/main.js
+++ src/main.js
@@ -1,25 +1,25 @@
-import Vue from 'vue'
-import axios from 'axios';
-import VueAxios from 'vue-axios';
-import App from './App.vue' 
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
 
-Vue.use(require('vue-moment'));
+Vue.use(require("vue-moment"));
 Vue.use(VueAxios, axios);
 
-// Adding  API URL base 
+// Adding  API URL base
 
 // Mongo Atlas
-// axios.defaults.baseURL = process.env.VUE_APP_KEY_CLOUD
-axios.defaults.baseURL = process.env.VUE_APP_KEY_LOCAL
+axios.defaults.baseURL = process.env.VUE_APP_KEY_CLOUD;
+// axios.defaults.baseURL = process.env.VUE_APP_KEY_LOCAL
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
